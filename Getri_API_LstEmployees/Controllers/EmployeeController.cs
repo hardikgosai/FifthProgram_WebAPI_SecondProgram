65fc82a1afde4bc3a3701108bff8c533af1bfdd7
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-13 22:22:38 +0530

    Add project files.

---
+++ Getri_API_LstEmployees/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using Getri_API_LstEmployees.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Getri_API_LstEmployees.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly List<Employee> lstEmployees = new List<Employee>();
+
+        public EmployeeController()
+        {            
+            lstEmployees.Add(new Employee() { EmpId = 101, EmpName = "Jimmy", EmpGender = "Male", EmpAge = 20});
+            lstEmployees.Add(new Employee() { EmpId = 102, EmpName = "Raj", EmpGender = "Male", EmpAge = 25});
+            lstEmployees.Add(new Employee() { EmpId = 103, EmpName = "Ricky", EmpGender = "Female", EmpAge = 23 });
+            lstEmployees.Add(new Employee() { EmpId = 104, EmpName = "Cris", EmpGender = "Male", EmpAge = 27 });
+            lstEmployees.Add(new Employee() { EmpId = 105, EmpName = "Jennifer", EmpGender = "Female", EmpAge = 22 });          
+        }
+
+        [HttpGet("FetchEmployees")]
+        public IEnumerable<Employee> GetRecords()
+        {
+               return lstEmployees;
+        }
+
+        [HttpGet("{id}", Name = "FetchEmployeeById")]
+        public Employee FetchById(int id)
+        {
+            return lstEmployees.FirstOrDefault(e => e.EmpId == id);            
+        }
+    }
+}
---
+++ Getri_API_LstEmployees/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Getri_API_LstEmployees.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpAge { get; set; }
+        public string EmpGender { get; set; }
+    }
+}
